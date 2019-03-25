d2277b58a5ad5910d34607fa28c75a6b70acb31c
zekai <1015346403@qq.com>
2019-03-25 20:43:52 +0800

    emmm

--- js/introduction.js
+++ js/introduction.js
@@ -7,3 +7,13 @@ function Skip() {
 function Back() {
   window.location.href = "start-page.html";
 }
+
+var height = window.getComputedStyle(document.getElementById("long")).height;
+var p = document.getElementsByTagName("p");
+//强行让文字居中（
+for(i = 0;i<p.length;i++){
+  var Height = window.getComputedStyle(p[i]).height;
+  var num = (parseInt(height)- parseInt(Height))/2;
+  p[i].style.paddingTop = num + "px";
+  p[i].style.height = parseInt(height) - num +"px";
+}
